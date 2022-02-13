d6347a4bf35142c6f771dc8788c131acf245b948
fer0561 <fer-gar-co@hotmail.com>
2022-02-13 18:02:23 +0100

    confirm

---
+++ conversiones.py
@@ -0,0 +1,10 @@
+num1 = 5.8
+num2 = int(num1)
+print(num2)
+
+edad = input("Dime tu edad: ")
+print(type(edad))
+edad = (int(edad))
+print(type(edad))
+nueva_edad = 1 + edad
+print("Tu nueva edad es " + str(nueva_edad))
---
+++ format.py
@@ -0,0 +1,4 @@
+color = "rojo"
+matricula = 562316
+
+print(f"El auto es {color} y la matrícula es {matricula}")
